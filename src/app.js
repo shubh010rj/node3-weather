9791852e6f8e0636a5206eb3cf38efd5899ab829
unknown <shuraj010@gmail.com>
2021-12-24 13:31:39 +0530

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ const hbs = require('hbs')
 const { hasSubscribers } = require('diagnostics_channel')
 //console.log(path.join(__dirname,'/../public'))
 const app = express()
+const port = process.env.PORT || 3000
+
 const viewsPath = path.join(__dirname,'../templates/views')
 const partialsPath = path.join(__dirname,'../templates/partials')
 app.set('view engine','hbs')
@@ -84,6 +86,6 @@ app.get('*',(req,res) =>{
     })
 })
 
-app.listen(3000,() =>{
-    console.log('server is up 3000')
+app.listen(port,() =>{
+    console.log('server is up ' + port)
 })
